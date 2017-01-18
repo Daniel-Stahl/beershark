c6009b5993c6b0db3d012d18b9c0b493fb817c4e
Daniel Stahl <danny.stahl@gmail.com>
2017-01-18 13:56:21 -0800

    Add member registration

---
+++ spec/features/member_registration_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "member registers for site" do
+	scenario "host registers" do
+		visit root_path
+		click_link "Become a Barkeep"
+		fill_in_registration_fields
+		expect(page).to have_content("Welcome! You have signed up successfully.")
+	end
+
+	def fill_in_registration_fields
+		fill_in "member[first_name]", with: Faker::Name.first_name
+		fill_in "member[last_name]", with: Faker::Name.last_name
+		fill_in "member[email]", with: Faker::Internet.email
+		fill_in "member[password]", with: Devise.friendly_token.first(8)
+		click_button "Sign up"
+	end
+end
