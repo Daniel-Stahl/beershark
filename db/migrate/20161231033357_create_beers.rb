c36c76a2b2a8697710fcf5c2ba9378b2ef8f7c17
Daniel Stahl <danny.stahl@gmail.com>
2017-04-21 12:31:11 -0700

    fix migration

--- db/migrate/20161231033357_create_beers.rb
+++
@@ -1,8 +0,0 @@
-class CreateBeers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :beers do |t|
-    	t.references :bar_keep, foreign_key: true
-      	t.timestamps
-    end
-  end
-end
