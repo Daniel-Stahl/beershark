fab4c9d97e2306897d598fcf4928814e13140366
Daniel Stahl <danny.stahl@gmail.com>
2017-04-21 12:24:18 -0700

    fix migration timestamp

--- db/migrate/20161230233457_create_bars.rb
+++
@@ -1,8 +0,0 @@
-class CreateBars < ActiveRecord::Migration[5.0]
-  def change
-    create_table :bars do |t|
-    	t.references :bar_keep, foreign_key: true
-      	t.timestamps
-    end
-  end
-end
